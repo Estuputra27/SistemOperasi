d787c0be90373d04d6531ff9c5fb69f63dcbd612
Rahmat M. Samik-Ibrahim <rahmatm.samik-ibrahim@vlsm.org>
2018-08-27 20:28:23 +0700

    rmsbase UF-ostmp

---
+++ demos/Week00/c-program-example.c
@@ -0,0 +1,28 @@
+/* 
+ * Copyright (C) 2018 Rahmat M. Samik-Ibrahim
+ * http://RahmatM.Samik-Ibrahim.vLSM.org/
+ * This free software is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * REV02 Mon Aug 27 18:17:11 WIB 2018
+ * REV01 Sun Aug 20 15:01:12 WIB 2017
+ * START Fri Jan 01 00:00:00 WIB 2016
+ *
+ * To compile:
+ * $ gcc -o c-program-example c-program-example.c
+ * To execute:
+ * $ ./c-program-example
+ *
+ * Note (RPS & AII)
+ * o Line 'include <stdio.h>' calls standard input/output library for C language.
+ * o 'void main()' means it's the main function where the program start its execution. 
+ * o This function returns nothing (or void).
+ * o function printf() will print the string inside the parentheses
+ */
+
+#include <stdio.h>
+
+void main() {
+   printf("This is program #1\n");
+}
+
