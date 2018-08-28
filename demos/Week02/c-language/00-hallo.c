5b64adf109a74c5bc3172c71026e09dd72aac4eb
Rahmat M. Samik-Ibrahim <rahmatm.samik-ibrahim@vlsm.org>
2018-08-28 21:01:11 +0700

    rmsbase demos

---
+++ demos/Week02/c-language/00-hallo.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2016-2018 Rahmat M. Samik-Ibrahim
+ * http://rahmatm.samik-ibrahim.vlsm.org/
+ * This program is free script/software. This program is distributed in the 
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * REV03 Tue Aug 28 13:31:19 WIB 2018
+ * START Tue Sep 13 11:51:21 WIB 2016
+ */
+
+#include <stdio.h>
+
+void main(void) {
+   printf("Hallo\n");
+}
+
+/*
+ * TAKE NOTE (AII)
+ * When you run this program, function printf() will 
+ * print the string "Hallo\n" to console.
+ */
+
