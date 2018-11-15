89676501921f20d792c3f36a87c5ee6d8c264cf1
Rahmat M. Samik-Ibrahim <rahmatm.samik-ibrahim@vlsm.org>
2018-11-15 12:17:55 +0700

    rmsbase Week08

--- demos/Week08/11-create-mmap.c
+++ demos/Week08/11-create-mmap.c
@@ -5,7 +5,7 @@
  * purpose, even commercially).  This program is distributed in the hope that 
  * it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * REV00 Wed Nov 14 20:30:05 WIB 2018
+ * REV01 Thu Nov 15 11:16:22 WIB 2018
  * START Wed Nov 14 20:30:05 WIB 2018
  */
 
@@ -16,6 +16,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
+#define MYFLAGS      O_RDWR
+#define MYPROTECTION PROT_READ|PROT_WRITE
+#define MYVISIBILITY MAP_SHARED
+char*  sfile="demo-file.bin";
 
 typedef  struct {
    char  mystring[16];
@@ -23,25 +27,28 @@ typedef  struct {
    char  ends[2];
 } myshare;
 
-#define MYFLAGS      O_RDWR
-#define MYPROTECTION PROT_READ|PROT_WRITE
-#define MYVISIBILITY MAP_SHARED
-char*  sfile="demo-file.bin";
-
 void main(void) {
-   printf("TBA1:create mmap\n");
-   int ssize=sizeof(myshare);
-   int fd=open(sfile,MYFLAGS,S_IRWXU);
+   struct stat fsize;
+   int    ssize=sizeof(myshare);
+   int    fd=open(sfile,MYFLAGS,S_IRWXU);
+
+   printf("Create mmap()\n");
+   fstat(fd, &fsize);
+   if (fsize.st_size < ssize) {
+      printf("Does %s exist? Size >= %d?\n", sfile, ssize);
+      exit(1);
+   }
    myshare* mymap=mmap(NULL, ssize, MYPROTECTION, MYVISIBILITY, fd, 0);
-   printf("TBA2:create mmap\n");
-   if (mymap == MAP_FAILED) exit(1);
-   printf("TBA3:create mmap\n");
+   if (mymap == MAP_FAILED) {
+      printf("mmap(): FAILED\n");
+      exit(1);
+   }
    mymap->mystring[0]='X';
    mymap->mystring[1]='Y';
    mymap->mystring[2]='Z';
    mymap->mystring[3]=' ';
    mymap->myint=0x61626364;
    close(fd);
-   printf("TBA6:create mmap\n");
+   printf("Please check  file \"%s\"\n", sfile);
 }
 
