cc30a624bc42dab10a1d7ff03b376ccfa9d6d5b7
Rahmat M. Samik-Ibrahim <rahmatm.samik-ibrahim@vlsm.org>
2019-01-22 21:06:16 +0700

    rmsbase Week06

--- demos/Week06/06d-fork.c
+++ demos/Week06/06d-fork.c
@@ -1,9 +1,10 @@
 /*
- * Copyright (C) 2005-2018 Rahmat M. Samik-Ibrahim
+ * Copyright (C) 2005-2019 Rahmat M. Samik-Ibrahim
  * http://rahmatm.samik-ibrahim.vlsm.org/
  * This program is free script/software. This program is distributed in the 
  * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * REV07 Tue Jan 22 21:03:46 WIB 2019
  * REV06 Wed Aug 29 16:11:46 WIB 2018
  * REV05 Wed Nov  1 13:34:33 WIB 2017
  * REV00 Mon Oct 24 10:43:00 WIB 2016
@@ -19,17 +20,16 @@
 void main(void) {
    pid_t val1, val2, val3;
    val3 = val2 = val1 = 1000;
-   printf("PID==%4d ==== ==== ==== ====\n", getpid());
+   printf("PID==[%5.5d] ==== ======= ==== =======\n", getpid());
+   // THREE FORK()s //
    fflush(NULL);
-/* ***** ***** ***** ***** START BLOCK *
    val1 = fork();
    wait(NULL);
    val2 = fork();
    wait(NULL);
    val3 = fork();
    wait(NULL);
-   ***** ***** ***** ***** END** BLOCK */
-   printf("VAL1=%4d VAL2=%4d VAL3=%4d\n", val1, val2, val3);
+   printf("VAL1=[%5.5d] VAL2=[%5.5d] VAL3=[%5.5d]\n", val1, val2, val3);
 }
 
 /*
