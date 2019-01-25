718e3e7a831201f29e660d5bc1e41f3f2c0a653a
Rahmat M. Samik-Ibrahim <rahmatm.samik-ibrahim@vlsm.org>
2019-01-25 18:29:43 +0700

    rmsbase c-language

--- demos/Week02/c-language/02-printf-etc.c
+++ demos/Week02/c-language/02-printf-etc.c
@@ -4,7 +4,7 @@
  * This program is free script/software. This program is distributed in the 
  * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * REV03 Wed Jan 23 17:48:33 WIB 2019
+ * REV04 Fri Jan 25 17:22:31 WIB 2019
  * REV02 Tue Aug 28 13:31:19 WIB 2018
  * START Thu Sep 15 11:47:10 WIB 2016
  */
@@ -14,13 +14,37 @@
 #define STRING "123456789ABCDEF\n"
 
 void main (void) {
-   char  cc='A';                // Character
+   short             sii    = 1;
+   unsigned short    usii   = sii * -1;
+   printf("short     %hd |||| unsigned short     %hu\n", sii, usii);
+
+   int               ii     = 1;
+   unsigned          uii    = ii * -1;
+   printf("int       %d |||| unsigned           %u\n", ii, uii);
+
+   long              lii    = 1;
+   unsigned long     ulii   = lii * -1;
+   printf("long      %ld |||| unsigned long      %lu (%#lX)\n", lii, ulii, ulii);
+
+   long long         llii   = 1;
+   unsigned long long ullii = llii * -1;
+   printf("long long %lld |||| unsigned long long %llu (%#llX)\n", llii, ullii, ullii);
+
+   float            ff    = 123456789.12345678;
+   printf("float       %f (%e)\n", ff, ff);
+   double            dff  = 123456789.12345678;
+   printf("double      %lf (%le)\n", dff, dff);
+   long double       ldff = 123456789.12345678;
+   printf("long double %Lf (%Le)\n", ldff, ldff);
+
+   char        ch='A';
+   unsigned char uch = (unsigned char) -191;
+   printf("char %c (%d) |||| unsigned char      %c (%u)\n", ch, ch, uch, uch);
+
    char  dd[]=STRING;
    long  size=sizeof(STRING);
-
    printf(STRING);
-   printf("AGAIN: %sSIZE=%ld\n", dd, size);
-   printf("cc=%c\n", cc);
+   printf("One more  time:\n%sSIZE=%ld\n", dd, size);
    printf("dd[]= %c%c%c ... %c\n", dd[0], 
            dd[1], dd[2], dd[size-3]);
 }
