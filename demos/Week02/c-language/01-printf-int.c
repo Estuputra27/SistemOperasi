446138d2a404a69826cd0d8111b3b9c0b6416ca0
Rahmat M. Samik-Ibrahim <rahmatm.samik-ibrahim@vlsm.org>
2019-02-26 09:36:05 +0700

    rmsbase c-language

--- demos/Week02/c-language/01-printf-int.c
+++ demos/Week02/c-language/01-printf-int.c
@@ -4,7 +4,7 @@
  * This program is free script/software. This program is distributed in the 
  * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * REV03 Wed Jan 23 17:47:41 WIB 2019
+ * REV04 Tue Feb 26 09:34:37 WIB 2019
  * REV02 Tue Aug 28 13:31:19 WIB 2018
  * START Thu Sep 15 11:47:10 WIB 2016
  */
@@ -12,20 +12,20 @@
 #include <stdio.h>
 
 void main (void) {
-   int   ii   = 74;             // Integer
+   int   ii   = 65;             // Integer
 
-   printf("ii = %d\n", ii);
-   printf("ii = %5d\n", ii);
-   printf("ii = %2d\n", ii);
-   printf("ii = %-5d\n", ii);
-   printf("ii = %5.5d\n", ii);
-   printf("ii = %5.3d\n", ii);
-   printf("ii = %c\n", ii);
-   printf("ii = %x\n", ii);
-   printf("ii = %#x\n", ii);
-   printf("ii = %X\n", ii);
-   printf("ii = %#X\n", ii);
-   printf("ii = %o\n", ii);
-   printf("ii = %#o\n", ii);
+   printf("(%%d)    ii = (%d)\n",    ii);
+   printf("(%%5d)   ii = (%5d)\n",   ii);
+   printf("(%%1d)   ii = (%1d)\n",   ii);
+   printf("(%%-5d)  ii = (%-5d)\n",  ii);
+   printf("(%%5.5d) ii = (%5.5d)\n", ii);
+   printf("(%%5.3d) ii = (%5.3d)\n", ii);
+   printf("(%%c)    ii = %c\n",      ii);
+   printf("(%%x)    ii = %x\n",      ii);
+   printf("(%%#x)   ii = %#x\n",     ii);
+   printf("(%%X)    ii = %X\n",      ii);
+   printf("(%%#X)   ii = %#X\n",     ii);
+   printf("(%%o)    ii = %o\n",      ii);
+   printf("(%%#o)   ii = %#o\n",     ii);
 }
 
