16f64feab62266a305080c2352680ca6172ef6a4
Rahmat M. Samik-Ibrahim <rahmatm.samik-ibrahim@vlsm.org>
2020-02-07 00:10:35 +0700

    rmsbase Week00

--- Demos/Week00/c-program-example.c
+++ Demos/Week00/c-program-example.c
@@ -1,18 +1,18 @@
 /* 
- * Copyright (C) 2016-2019 Rahmat M. Samik-Ibrahim
+ * Copyright (C) 2016-2020 Rahmat M. Samik-Ibrahim
  * http://RahmatM.Samik-Ibrahim.vLSM.org/
  * This free software is distributed in the hope that it will be
  * useful, but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * REV05 Thu Feb  6 23:50:54 WIB 2020
  * REV04 Wed Feb 27 19:14:51 WIB 2019
  * REV02 Mon Aug 27 18:17:11 WIB 2018
  * REV01 Sun Aug 20 15:01:12 WIB 2017
  * START Fri Jan 01 00:00:00 WIB 2016
- *
- * To compile:
- * $ gcc -o c-program-example c-program-example.c
- * To execute:
- * $ ./c-program-example
+# INFO:  To compile, run:
+# INFO:                 gcc -o c-program-example c-program-example.c
+# INFO:               To execute, run:
+# INFO:                 ./c-program-example
  *
  * Note (RPS & AII)
  * o Line 'include <stdio.h>' calls standard input/output library for C language.
