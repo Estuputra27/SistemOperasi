fcc51a79d28ebe6d2159c2cd697e19ec2998dfa2
Rahmat M. Samik-Ibrahim <rahmatm.samik-ibrahim@vlsm.org>
2020-05-03 09:18:02 +0700

    pamulang1 UF-SistemOperasi

--- Demos/Week10/76-os192.c
+++ Demos/Week10/76-os192.c
@@ -5,6 +5,7 @@
  * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
+ * REV03 Sun May  3 09:17:12 WIB 2020
  * REV02 Wed Mar 25 17:00:18 WIB 2020
  * REV01 XXX Dec 17 20:00:00 WIB 2019
  * START XXX Dec 16 15:55:00 WIB 2019
@@ -20,12 +21,13 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
-#define FILE1 "zdemo1.txt"
+#define FILE    "76-os192-demo.txt"
 
 void main(void) {
+   printf("See %s\n", FILE);
    printf ("OUT=%d\n", STDOUT_FILENO);
    close(STDOUT_FILENO);
-   int fd1 = open (FILE1, O_RDWR | 
+   int fd1 = open (FILE, O_RDWR | 
               O_CREAT | O_TRUNC, 0644);
    int fd2 = dup2(fd1, 9);
    printf(         "A\n"); 
@@ -33,8 +35,4 @@ void main(void) {
    dprintf(fd2,    "fd1=%d\nfd2=%d\n", 
                             fd1, fd2);
 }
-
-//       1         2         3         4
-// 4567890123456789012345678901234567890
-
 
